2f3229ccb19e1e3f67a871665b6516c784f892a6
susumu tomita <oyster880@gmail.com>
2022-07-26 11:10:36 +0900

    Add tests

---
+++ demo/src/test/kotlin/com/example/demo/controller/HelloControllerTests.kt
@@ -0,0 +1,29 @@
+package com.example.demo.controller
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
+import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.http.*
+import org.springframework.web.client.RestTemplate
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class HelloControllerTests {
+@LocalServerPort
+private val port =8080
+
+private val restTemplate: RestTemplate = RestTemplate()
+
+        @Test
+        @Throws(Exception::class)
+        fun helloTest() {
+            val url ="http://localhost:$port/"
+            val headers = HttpHeaders()
+            headers.contentType = MediaType.APPLICATION_JSON
+            val entity:HttpEntity<Any> = HttpEntity<Any>(headers)
+            val response:ResponseEntity<String> = restTemplate.exchange(url,HttpMethod.GET,entity,String::class.java)
+            Assertions.assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
+        }
+
+}
